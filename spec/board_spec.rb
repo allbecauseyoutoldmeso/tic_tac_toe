669a56964f7e771025d051f4b527097a4ad2733b
Kate <allbecauseyoutoldmeso@yahoo.co.uk>
2017-05-17 18:46:37 +0100

    more tests updated

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -4,6 +4,20 @@ describe Board do
 
   subject(:board) { described_class.new }
 
+  describe '#cell_taken' do
+    it 'is true if cell is taken' do
+      board.grid[0][0] = 'x'
+      expect(board.cell_taken?(1,1)).to be_truthy
+    end
+  end
+
+  describe '#take_cell' do
+    it 'sets a cell to x or o' do
+      board.take_cell(1,1,'x')
+      expect(board.grid[0][0]).to eq 'x'
+    end
+  end
+
   describe '#wins?' do
     it 'returns true if player has whole row' do
       board.grid[0][0] = 'x'
