284a2de8314cdbcbb6ff2807b9b64e1916a87901
Kate <allbecauseyoutoldmeso@yahoo.co.uk>
2017-05-17 16:33:06 +0100

    extract occupied column and row methods

--- lib/game.rb
+++ lib/game.rb
@@ -3,7 +3,7 @@ require_relative 'player'
 
 class Game
 
-  attr_reader :board
+  attr_reader :board, :row_1, :row_2, :row_3
   attr_accessor :current_player
 
   def initialize
@@ -20,8 +20,8 @@ class Game
 
   def take(x,y)
     raise 'cell already taken' if @board[x-1][y-1] != ''
-    @board[x-1][y-1] = @current_player
-    winner? ? puts("#{@current_player} wins!") : switch_player
+    @board[x-1][y-1] = current_player
+    winner? ? puts("#{current_player} wins!") : switch_player
   end
 
   def winner?
@@ -31,15 +31,23 @@ class Game
   end
 
   def row_win?
-    @row_1.all? { |cell| cell == @current_player } || @row_1.all? { |cell| cell == @current_player } || @row_1.all? { |cell| cell == @current_player }
+    occupied_row?(row_1) || occupied_row?(row_2) || occupied_row?(row_3)
+  end
+
+  def occupied_row?(row)
+    row.all? { |cell| cell == current_player }
   end
 
   def column_win?
-    [@row_1[0], @row_2[0], @row_3[0]].all? { |cell| cell == @current_player } || [@row_1[1], @row_2[1], @row_3[1]].all? { |cell| cell == @current_player } || [@row_1[2], @row_2[2], @row_3[2]].all? { |cell| cell == @current_player }
+    occupied_column?(0) || occupied_column?(1) || occupied_column?(2)
+  end
+
+  def occupied_column?(column)
+    [row_1[column], row_2[column], row_3[column]].all? { |cell| cell == current_player }
   end
 
   def diagonal_win?
-    [@row_1[0], @row_2[1], @row_3[2]].all? { |cell| cell == @current_player } || [@row_1[2], @row_2[1], @row_3[0]].all? { |cell| cell == @current_player }
+    [row_1[0], row_2[1], row_3[2]].all? { |cell| cell == current_player } || [row_1[2], row_2[1], row_3[0]].all? { |cell| cell == current_player }
   end
 
 end
