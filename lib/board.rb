2a00c442e4c06d9de0521fe37a4f9f8fa92b4b8e
Kate <allbecauseyoutoldmeso@yahoo.co.uk>
2017-05-18 13:35:23 +0100

    refactor column_win method

--- lib/board.rb
+++ lib/board.rb
@@ -34,12 +34,13 @@ class Board
   # end
 
   def column_win?(player)
-    occupied_column?(0, player) || occupied_column?(1, player) || occupied_column?(2, player)
+    [0, 1, 2].any? { |column| grid.all? { |row| row[column] == player } }
+    # occupied_column?(0, player) || occupied_column?(1, player) || occupied_column?(2, player)
   end
 
-  def occupied_column?(column, player)
-    [grid[0][column], grid[1][column], grid[2][column]].all? { |cell| cell == player }
-  end
+  # def occupied_column?(column, player)
+  #   [grid[0][column], grid[1][column], grid[2][column]].all? { |cell| cell == player }
+  # end
 
   def diagonal_win?(player)
     [grid[0][0], grid[1][1], grid[2][2]].all? { |cell| cell == player } || [grid[0][2], grid[1][1], grid[2][0]].all? { |cell| cell == player }
